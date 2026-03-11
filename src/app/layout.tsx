9788cc4368649bc74b28483c0393a5e93516bfa0
Diogo Santana <238937366+diogosax@users.noreply.github.com>
2026-03-11 18:48:21 -0300

    feat: enable vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
@@ -28,6 +29,7 @@ export default function RootLayout({
         <Header />
         <main>{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
